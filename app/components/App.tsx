b0dbeb740758afdb6db35d1fd3be3a7397ab4581
Zhijin Xiang <z5518180@unsw.edu.au>
2025-01-20 16:28:06 +1100

    Keep all transcripts

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -15,13 +15,10 @@ import {
 import Visualizer from "./Visualizer";
 
 const App: () => JSX.Element = () => {
-  const [caption, setCaption] = useState<string | undefined>(
-    "Powered by Deepgram"
-  );
+  const [transcripts, setTranscripts] = useState<string[]>([]);
   const { connection, connectToDeepgram, connectionState } = useDeepgram();
   const { setupMicrophone, microphone, startMicrophone, microphoneState } =
     useMicrophone();
-  const captionTimeout = useRef<any>();
   const keepAliveInterval = useRef<any>();
 
   useEffect(() => {
@@ -55,21 +52,13 @@ const App: () => JSX.Element = () => {
     };
 
     const onTranscript = (data: LiveTranscriptionEvent) => {
-      const { is_final: isFinal, speech_final: speechFinal } = data;
+      const { is_final: isFinal } = data;
       let thisCaption = data.channel.alternatives[0].transcript;
 
-      console.log("thisCaption", thisCaption);
       if (thisCaption !== "") {
-        console.log('thisCaption !== ""', thisCaption);
-        setCaption(thisCaption);
-      }
-
-      if (isFinal && speechFinal) {
-        clearTimeout(captionTimeout.current);
-        captionTimeout.current = setTimeout(() => {
-          setCaption(undefined);
-          clearTimeout(captionTimeout.current);
-        }, 3000);
+        if (isFinal) {
+          setTranscripts(prev => [...prev, thisCaption]);
+        }
       }
     };
 
@@ -84,7 +73,6 @@ const App: () => JSX.Element = () => {
       // prettier-ignore
       connection.removeListener(LiveTranscriptionEvents.Transcript, onTranscript);
       microphone.removeEventListener(MicrophoneEvents.DataAvailable, onData);
-      clearTimeout(captionTimeout.current);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [connectionState]);
@@ -116,11 +104,16 @@ const App: () => JSX.Element = () => {
       <div className="flex h-full antialiased">
         <div className="flex flex-row h-full w-full overflow-x-hidden">
           <div className="flex flex-col flex-auto h-full">
-            {/* height 100% minus 8rem */}
             <div className="relative w-full h-full">
               {microphone && <Visualizer microphone={microphone} />}
-              <div className="absolute bottom-[8rem]  inset-x-0 max-w-4xl mx-auto text-center">
-                {caption && <span className="bg-black/70 p-8">{caption}</span>}
+              <div className="absolute bottom-[8rem] inset-x-0 max-w-4xl mx-auto text-center">
+                <div className="bg-black/70 p-8 max-h-[60vh] overflow-y-auto">
+                  {transcripts.map((transcript, index) => (
+                    <div key={index} className="mb-2">
+                      {transcript}
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
